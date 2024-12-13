b31277f490089c0cbdec91e438a73bf320823810
LinuxMacin <my.linux.macine@gmail.com>
2024-12-12 22:44:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const intro = document.querySelector("top-Image");
 let scrollY = window.scrollY;
-const video = document.querySelector('video'); // select the video element
+const video = document.querySelector('video'); // select the video element 
 
 // Set playback rate to 2 (twice the normal speed)
 video.playbackRate = 2;
@@ -16,17 +16,23 @@ window.addEventListener("scroll", ()=>{
     }
 })
 
-myID = document.getElementById("myPic");
+myID = document.getElementById("container");
 fill = document.getElementById("fil");
+card = document.getElementById("ca");
 
 var myScrollFunc = function () {
     var y = window.scrollY;
-    if (y >= 400) {
+    if (y >= 250) {
         myID.className = "top-Image show"
         fill.className = "fill show"
-    } else if(y <= 100){
+    } else if(y <= 250){
         fill.className = "fill hide"
         myID.className = "top-Image hide"
+    }
+    if(y>=700){
+        fill.className = "fill hide"
+        myID.className = "top-Image hide"
+        card.className = "revolve show"
     }
 };
 
